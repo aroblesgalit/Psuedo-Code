980d8e0648982ef64a297feb76fe049844256a52
Alvin Galit <aroblesgalit@gmail.com>
2020-03-17 11:35:11 -0500

    initial commit

---
+++ logic.js
@@ -0,0 +1,28 @@
+// * With a partner, spend a few moments outlining all the steps and conditions that go into a single game of rock paper scissors.
+
+// * Try to break it down into steps that you could code out.
+
+// * Think of basic elements like loops, if-else statements, arrays, alerts, etc.
+
+
+// * Be prepared to share your outlines approach.
+
+// User chooses rock, paper, scissor
+var user = prompt("Do you choose rock, paper, or scissors?");
+
+// Create array of Rock, Paper, Scissors
+var array = ["rock", "paper", "scissors"];
+
+// Use Math.random() to choose from array
+var compChoice = Math.floor(Math.random() * 2);
+console.log(compChoice);
+
+
+// Alert chosen item using if/else statement
+if (user === "rock") {
+
+} else if (user === "paper") {
+
+} else if (user === "scissors") {
+
+}
