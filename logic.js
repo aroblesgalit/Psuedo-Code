9c8387d0755c40b4c1059d371dda42c6f9710ac7
Alvin Galit <aroblesgalit@gmail.com>
2020-03-17 12:03:09 -0500

    added if else statements into loop

--- logic.js
+++ logic.js
@@ -7,8 +7,6 @@
 
 // * Be prepared to share your outlines approach.
 
-// User chooses rock, paper, scissor
-var user = prompt("Do you choose rock, paper, or scissors?");
 
 // Create array of Rock, Paper, Scissors
 var array = ["rock", "paper", "scissors"];
@@ -17,12 +15,48 @@ var array = ["rock", "paper", "scissors"];
 var compChoice = Math.floor(Math.random() * 2);
 console.log(compChoice);
 
+// Keep Score
+var wins = 0;
+var loses = 0;
+var ties = 0;
 
 // Alert chosen item using if/else statement
-if (user === "rock") {
+// Have users play the game 10 times, then show them their total scores.
+for (var i = 0; i < 10; i++) {
 
-} else if (user === "paper") {
+    // User chooses rock, paper, scissor
+    var user = prompt("Do you choose rock, paper, or scissors?");
 
-} else if (user === "scissors") {
+    if (user === "rock") {
+
+        if (compChoice === 0) {
+            alert("Computer chooses " + array[0] + ". It's a tie!");
+        } else if (compChoice === 1) {
+            alert("Computer chooses " + array[1] + ". Computer wins!");
+        } else {
+            alert("Computer chooses " + array[2] + ". You win!");
+        }
+    
+    } else if (user === "paper") {
+    
+        if (compChoice === 0) {
+            alert("Computer chooses " + array[0] + ". You win!");
+        } else if (compChoice === 1) {
+            alert("Computer chooses " + array[1] + ". It's a tie!");
+        } else {
+            alert("Computer chooses " + array[2] + ". Computer wins!");
+        }
+    
+    } else if (user === "scissors") {
+    
+        if (compChoice === 0) {
+            alert("Computer chooses " + array[0] + ". Computer wins!");
+        } else if (compChoice === 1) {
+            alert("Computer chooses " + array[1] + ". You win!");
+        } else {
+            alert("Computer chooses " + array[2] + ". It's a tie!");
+        }
+
+    }
 
 }
