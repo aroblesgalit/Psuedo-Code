8a26e094d48eb74b0aef62ad31c7982467b55d92
Alvin Galit <aroblesgalit@gmail.com>
2020-03-17 12:55:53 -0500

    updated var name loses to losses

--- logic.js
+++ logic.js
@@ -3,7 +3,7 @@ var array = ["rock", "paper", "scissors"];
 
 // Keep Score
 var wins = 0;
-var loses = 0;
+var losses = 0;
 var ties = 0;
 
 // Have users play the game 10 times, then show them their total scores.
@@ -14,6 +14,7 @@ for (var i = 0; i < 10; i++) {
 
     // Use Math.random() to choose from array
     var compChoice = Math.floor(Math.random() * 3);
+    console.log(compChoice);
 
     // Alert chosen item using if/else statement
     if (user === "rock") {
@@ -23,7 +24,7 @@ for (var i = 0; i < 10; i++) {
             ties++;
         } else if (compChoice === 1) {
             alert("Computer chooses " + array[1] + ". Computer wins!");
-            loses++;
+            losses++;
         } else {
             alert("Computer chooses " + array[2] + ". You win!");
             wins++;
@@ -39,14 +40,14 @@ for (var i = 0; i < 10; i++) {
             ties++;
         } else {
             alert("Computer chooses " + array[2] + ". Computer wins!");
-            loses++;
+            losses++;
         }
 
     } else if (user === "scissors") {
 
         if (compChoice === 0) {
             alert("Computer chooses " + array[0] + ". Computer wins!");
-            loses++;
+            losses++;
         } else if (compChoice === 1) {
             alert("Computer chooses " + array[1] + ". You win!");
             wins++;
@@ -56,6 +57,6 @@ for (var i = 0; i < 10; i++) {
         }
 
     }
-    alert("Wins: " + wins + " -- Loses: " + loses + " -- Ties: " + ties);
+    alert("Wins: " + wins + " -- Loses: " + losses + " -- Ties: " + ties);
 
 }
